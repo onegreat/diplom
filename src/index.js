e25fe0a507c94f5bdfc539ce5a86cb1738f25763
Onegreat <wangreat@yandex.ru>
2023-01-19 21:34:25 +0300

    Реализация слайдера

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import modal from "./modules/modal";
 import linkAnim from "./modules/linkAnimation";
 import form from "./modules/form";
-// import slider from "./modules/slider";
+import slider from "./modules/slider";
 import scrollUp from "./modules/scrollUp";
 
 modal()
 linkAnim()
 form()
-// slider()
+slider()
 scrollUp()
 
