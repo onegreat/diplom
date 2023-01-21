08d15c6a95758c83a2da4d8a3a4031fca0c0886e
Onegreat <wangreat@yandex.ru>
2023-01-21 10:05:39 +0300

    Полностью реализовал скрол при открытии модального окна + правки css

--- src/modules/helpers.js
+++ src/modules/helpers.js
@@ -17,6 +17,19 @@ function animate({ timing, draw, duration }) {
         }
 
     });
+
 }
 
-export { animate }
+function calcScroll() {
+    let div = document.createElement('div');
+    div.style.width = '50px';
+    div.style.height = '50px';
+    div.style.overflowY = 'scroll';
+    div.style.visibility = 'hidden';
+    document.body.appendChild(div);
+    let scrollWidth = div.offsetWidth - div.clientWidth;
+    div.remove();
+    return scrollWidth;
+}
+
+export { animate, calcScroll }
--- src/modules/modalService.js
+++ src/modules/modalService.js
@@ -1,3 +1,4 @@
+import { calcScroll } from "./helpers";
 const modalService = () => {
     const btnService = document.querySelector('.button-services');
     const modal = document.querySelector('.modal-callback');
@@ -11,56 +12,46 @@ const modalService = () => {
 
     const scrollRemove = (isEnabled = true) => {
 
-        let div = document.createElement('div');
-
-        div.style.overflowY = 'scroll';
-        div.style.width = '50px';
-        div.style.height = '50px';
-        document.body.append(div);
-        let scrollWidth = div.offsetWidth - div.clientWidth;
-        console.log(scrollWidth);
-        div.remove();
-
-
 
         if (isEnabled) {
             body.classList.add('disable-scroll')
             body.dataset.position = pagePosY
             body.style.top = -pagePosY + 'px'
-            body.style.marginRight = scrollWidth + 'px'
+            body.style.paddingRight = calcScroll + 'px'
+
+
         } else {
             pagePosY = +body.dataset.position
             body.style.top = 'auto'
             body.classList.remove('disable-scroll')
             window.scroll({ top: pagePosY, left: 0 })
             body.removeAttribute('data-position')
-            body.style.margin = 0
+
         }
     }
 
-
-
     window.addEventListener('scroll', () => {
         pagePosY = window.scrollY
-
-
+        pagePosX = window.screen.width
     })
 
     slideBtn.forEach((item) => {
         item.addEventListener('click', function (e) {
             e.preventDefault()
             scrollRemove()
+            calcScroll()
+            body.style.paddingRight = calcScroll() + 'px'
             modal.style.display = 'block';
             overlay.style.display = 'block';
             modal.style.opacity = '1';
             btn.style.display = 'none';
-
 
         });
     });
     btnService.addEventListener('click', function (e) {
         e.preventDefault()
         scrollRemove()
+        body.style.paddingRight = calcScroll + 'px'
         modal.style.display = 'block';
         modal.style.opacity = '1';
         overlay.style.display = 'block';
@@ -70,6 +61,7 @@ const modalService = () => {
 
     function closeModal() {
         scrollRemove(false)
+        body.style.paddingRight = 0 + 'px'
         modal.style.display = 'none';
         overlay.style.display = 'none';
         btn.style.display = 'block';
